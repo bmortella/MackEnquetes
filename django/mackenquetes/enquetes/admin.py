eaf7af6ff12028231cc5d56999679e5e4fba57bc
Bruno Mortella <bmortella@gmail.com>
2018-09-18 18:14:01 -0300

    Melhorando Admin

--- django/mackenquetes/enquetes/admin.py
+++ django/mackenquetes/enquetes/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Pergunta, Escolha
 
 # Register your models here.
-admin.site.register(Pergunta)
-admin.site.register(Escolha)
+class EscolhaInline(admin.TabularInline):
+    model = Escolha
+    extra = 2
+
+class PerguntaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['enunciado']}),
+        ('Data', {'fields':['data_pub']})
+    ]
+    inlines = [EscolhaInline]
+    list_display = ('enunciado', 'data_pub')
+    list_filter = ['data_pub']
+    search_fields = ['enunciado']
+
+admin.site.register(Pergunta, PerguntaAdmin)
