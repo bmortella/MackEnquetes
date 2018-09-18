83e0be79fa714ad1f2894130e01e0d502729ac7d
Bruno Mortella <bmortella@gmail.com>
2018-09-18 15:08:38 -0300

    Criando modelos

--- django/mackenquetes/enquetes/models.py
+++ django/mackenquetes/enquetes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Pergunta(models.Model):
+    enunciado = models.CharField(max_length=200)
+    data_pub = models.DateTimeField("Data de publicação")
+
+    def __str__(self):
+        return self.enunciado
+
+class Escolha(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+    votos = models.IntegerField()
+
+    def __str__(self):
+        return self.texto
